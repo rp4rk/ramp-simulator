73d90d06044ada208605fdea885a240f0ff3311d
Ryan Park <admin@ryanpark.co.uk>
2021-06-13 21:41:32 +0100

    Remove unused ref

--- src/components/SpellSelection/index.tsx
+++ src/components/SpellSelection/index.tsx
@@ -1,7 +1,7 @@
 import { DragSpell } from "components/Spell";
 import { Spells } from "lib";
 import { Spell as SpellType, SpellCategory } from "lib/types";
-import { Separator, SelectionContainer, SelectionCategory } from "./styled";
+import { SelectionContainer, SelectionCategory } from "./styled";
 
 function set<T>(s: string, o: { [index: string]: T[] }, i: T) {
   if (o[s]) {
@@ -31,16 +31,14 @@ export const SpellSelection = function () {
       </div>
       <SelectionContainer>
         {Object.entries(SPELL_CATEGORIES).map(([key, spells]) => (
-          <>
-            <div>
-              <h5>{key}</h5>
-              <SelectionCategory>
-                {spells.map((spell) => (
-                  <DragSpell key={spell.id} spell={spell} />
-                ))}
-              </SelectionCategory>
-            </div>
-          </>
+          <div key={key}>
+            <h5>{key}</h5>
+            <SelectionCategory>
+              {spells.map((spell) => (
+                <DragSpell key={spell.id} spell={spell} />
+              ))}
+            </SelectionCategory>
+          </div>
         ))}
       </SelectionContainer>
     </>
