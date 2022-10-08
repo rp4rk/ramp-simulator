402f87f6c1d36df7a01ec996c34a13da121c1861
Ryan Park <admin@ryanpark.co.uk>
2022-10-08 21:46:51 +0100

    Add DF tag

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <header className="bg-white dark:bg-gray-900 p-6 mb-4">
-      <h1 className="font-semi-bold text-gray-500 dark:text-white text-xl tracking-tight">Ramp Simulator</h1>
+      <h1 className="font-semi-bold text-gray-500 dark:text-white text-xl tracking-tight">
+        Ramp Simulator - DF Edition
+      </h1>
     </header>
   );
 }
