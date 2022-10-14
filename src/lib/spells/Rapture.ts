af50c4c35f45b983b8fba72415084afdcf3ccf4b
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-10-14 22:51:26 +0200

    rapture now resets the cd of pws

--- src/lib/spells/Rapture.ts
+++ src/lib/spells/Rapture.ts
@@ -4,8 +4,17 @@ import { createManaCost } from "../mechanics/mana";
 import { hasTalent } from "../talents";
 import { applyAtonement } from "./Atonement";
 import { calculateShieldAbsorb } from "./PowerWordShield";
+import { cdr } from "lib/mechanics/cdr";
+import { StateSpellReducer } from "../types";
+
 
 export const RAPTURE_COEFFICIENT = 0.3;
+export const POWER_WORD_SHIELD_COOLDOWN_MS = 7500;
+
+export const resetPowerWordShieldCooldown: StateSpellReducer = (state, spell) => {
+  return cdr("Power Word: Shield", POWER_WORD_SHIELD_COOLDOWN_MS)(state, spell);
+};
+
 
 export const Rapture: Spell = {
   category: SpellCategory.Cooldown,
@@ -30,5 +39,6 @@ export const Rapture: Spell = {
     absorb,
     applyAtonement,
     advanceTime,
+    resetPowerWordShieldCooldown,
   ],
 };
