625cedb4b33490e841d3f8f10a504d5c68d51e88
oisin odoherty <oisin.odoherty@ucdconnect.ie>
2022-10-28 22:20:12 +0200

    stolen psyche module

--- src/lib/mechanics/atonement.ts
+++ src/lib/mechanics/atonement.ts
@@ -3,10 +3,11 @@ import { numBuffsActive } from "../buff";
 import { calculateDamage } from "./damage";
 import { getMasteryPerc } from "../player";
 import { createDivineAegisShield } from "../talents/DivineAegis";
-import { hasTalent } from "lib/talents";
+import { getTalentPoints, hasTalent } from "lib/talents";
 
 const ATONEMENT_COEFFICIENT = 0.5;
 
+const STOLEN_PSYCHE_ID = 373054;
 /**
  * Returns the simulation state with healing and absorbs from Atonement applied
  */
@@ -16,8 +17,11 @@ export const atonement: StateSpellReducer = (state, spell, tick): SimState => {
   const { player } = state;
   const mastery = getMasteryPerc(player);
 
+  const stolenPsychePoints = getTalentPoints(state, STOLEN_PSYCHE_ID);
+  const stolenPsycheBonus = spell.name === "Mind Blast" && hasTalent(state, STOLEN_PSYCHE_ID) ? 1 + 0.2 * stolenPsychePoints : 1;
+
   const calculatedHealing =
-    calculatedDamage * ATONEMENT_COEFFICIENT * activeAtonementCount * mastery;
+    calculatedDamage * ATONEMENT_COEFFICIENT * activeAtonementCount * mastery * stolenPsycheBonus;
 
   const wrathUnleashedBonus = spell.name === "Light's Wrath" && hasTalent(state, 390781) ? 0.15 : 0;
 
