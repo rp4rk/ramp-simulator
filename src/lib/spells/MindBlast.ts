cbd7d44eb55f14b0e63c404bdb28911339a6aaf0
Ryan Park <admin@ryanpark.co.uk>
2022-11-09 01:23:35 +0000

    Borrowed Time

--- src/lib/spells/MindBlast.ts
+++ src/lib/spells/MindBlast.ts
@@ -1,4 +1,4 @@
-import { advanceTime, atonement, damage } from "../mechanics";
+import { advanceTime, atonement, cooldown, damage } from "../mechanics";
 import { Spell, SpellCategory } from "../types";
 import { createManaCost } from "../mechanics/mana";
 import { InescapableTorment } from "../talents/InescapableTorment";
@@ -9,6 +9,7 @@ import {
 } from "lib/talents/TwilightEquilibrium";
 import { buildDamage } from "lib/mechanics/util/buildDamage";
 import { applyTwilightEquilibriumHoly } from "../talents/TwilightEquilibrium";
+import { getHastePerc } from "lib/player";
 
 export const MindBlast: Spell = {
   category: SpellCategory.Damage,
@@ -16,12 +17,18 @@ export const MindBlast: Spell = {
   icon: "spell_shadow_unholyfrenzy",
   name: "Mind Blast",
   cost: createManaCost(2.5),
+  cooldown: (state) => {
+    const haste = getHastePerc(state.player);
+
+    return 9000 / haste;
+  },
   damage: buildDamage(78.336 * 1.32, [
     twilightEquilibriumBuff(TwilightEquilibriumSchool.Shadow),
     expiationBuff,
   ]),
   castTime: 1500,
   effect: [
+    cooldown,
     advanceTime,
     damage,
     atonement,
