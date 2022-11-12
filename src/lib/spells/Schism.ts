4bff603795771dc25e177398962679db689fe685
Ryan Park <rp4rk@users.noreply.github.com>
2022-11-12 02:52:26 +0000

    Malicious Intent (#23)

--- src/lib/spells/Schism.ts
+++ src/lib/spells/Schism.ts
@@ -3,10 +3,24 @@ import { Spell, SpellCategory } from "../types";
 import { createManaCost } from "../mechanics/mana";
 import { buildDamage } from "lib/mechanics/util/buildDamage";
 import { applyTwilightEquilibriumHoly } from "../talents/TwilightEquilibrium";
+import { StateSpellReducer } from "lib/types";
 import {
   twilightEquilibriumBuff,
   TwilightEquilibriumSchool,
 } from "lib/talents/TwilightEquilibrium";
+import { hasTalent } from "lib/talents";
+
+const MALICIOUS_INTENT_ID = 372969;
+const MALICIOUS_INTENT_DURATION = 6000;
+
+const applySchismBuff: StateSpellReducer = (state) => {
+  const hasMaliciousIntent = hasTalent(state, MALICIOUS_INTENT_ID);
+
+  return applyAura(state, {
+    name: "Schism",
+    duration: 9000 + (hasMaliciousIntent ? MALICIOUS_INTENT_DURATION : 0),
+  });
+};
 
 export const Schism: Spell = {
   category: SpellCategory.Cooldown,
@@ -17,17 +31,5 @@ export const Schism: Spell = {
   damage: buildDamage(141, [twilightEquilibriumBuff(TwilightEquilibriumSchool.Shadow)]),
   castTime: 1500,
   cooldown: 24000,
-  effect: [
-    cooldown,
-    advanceTime,
-    damage,
-    atonement,
-    (state) => {
-      return applyAura(state, {
-        name: "Schism",
-        duration: 9000,
-      });
-    },
-    applyTwilightEquilibriumHoly,
-  ],
+  effect: [cooldown, advanceTime, damage, atonement, applySchismBuff, applyTwilightEquilibriumHoly],
 };
